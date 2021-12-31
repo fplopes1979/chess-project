27ad2a1306faf2c326096bf371d84d23f1d35840
Fabio Lopes <fpl@cesar.school>
2021-12-31 00:41:31 -0300

    First class: Position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
